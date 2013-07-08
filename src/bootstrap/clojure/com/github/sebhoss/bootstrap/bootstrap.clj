7095d18c0ef6e310118f3d6a4094df4d890f9441
Sebastian Hoß <mail@shoss.de>
2013-07-08 18:44:52 +0200

    Added bootstrap script to bootstrap the project called 'bootstrap'
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/bootstrap/clojure/com/github/sebhoss/bootstrap/bootstrap.clj
@@ -0,0 +1,7 @@
+(require '[com.github.sebhoss.bootstrap.repl :refer :all])
+
+;; Load project namespaces
+(load-namespaces #".*?bootstrap")
+
+;; 'refresh', 'doc', 'source', etc. support
+(load-helpers)
