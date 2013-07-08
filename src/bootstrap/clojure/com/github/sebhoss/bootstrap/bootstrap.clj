19a55b305f7ae3528a49abad56f42d28cd1f6555
Sebastian Hoß <mail@shoss.de>
2013-07-08 18:46:52 +0200

    Removed redundant ';'
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/bootstrap/clojure/com/github/sebhoss/bootstrap/bootstrap.clj
+++ src/bootstrap/clojure/com/github/sebhoss/bootstrap/bootstrap.clj
@@ -1,7 +1,7 @@
 (require '[com.github.sebhoss.bootstrap.repl :refer :all])
 
-;; Load project namespaces
+; Load project namespaces
 (load-namespaces #".*?bootstrap")
 
-;; 'refresh', 'doc', 'source', etc. support
+; 'refresh', 'doc', 'source', etc. support
 (load-helpers)
