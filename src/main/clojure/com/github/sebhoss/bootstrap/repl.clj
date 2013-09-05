a0585583133c5a4405fdb126a39a9d8222fe0975
Sebastian Hoß <mail@shoss.de>
2013-09-05 14:16:21 +0200

    Replaced macro with multi-aritity function.
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/clojure/com/github/sebhoss/bootstrap/repl.clj
+++ src/main/clojure/com/github/sebhoss/bootstrap/repl.clj
@@ -28,7 +28,7 @@
   (require '[clojure.repl :refer :all])
   (require '[clojure.test :refer :all]))
 
-(defmacro test-shortcut
+(defn test-shortcut
   "Defines a shortcut for (run-all-tests) called (rat) which can be called without any argument
    to execute all tests matching the given macro regex. Otherwise the given parameter for (rat)
    is used to determine which tests to run.
@@ -37,11 +37,12 @@
      * regex - Regular expression to match all namespaces which contain tests
 
    Example:
-     * (test-shortcut #\"foo.bar.*-test\")
+     * (def rat (test-shortcut #\"foo.bar.*-test\"))
        => Calling (rat) runs all tests in namespaces matching the above regex.
           Calling (rat \"baz\") runs all tests inside the 'foo.bar.baz-test' namespace."
   [regex]
-  (let [ns-regex (clojure.string/replace (str regex) #"\*" "%s")]
-    `(defn rat
-       ([] (run-all-tests ~regex))
-       ([~'namespace] (run-all-tests (re-pattern (format ~ns-regex ~'namespace)))))))
+  (fn
+    ([] (clojure.test/run-all-tests regex))
+    ([namespace]
+      (let [ns-regex (clojure.string/replace (str regex) #"\*" "%s")]
+        (clojure.test/run-all-tests (re-pattern (format ~ns-regex ~'namespace)))))))
